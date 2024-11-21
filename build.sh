27f6d60ba9a2b531f244e5c61327dec26b965cf0
Muhammad Ahsan <ahsan@vivaldi.net>
2024-11-21 01:53:37 +0100

    Streamline code to conform shellcheck and various improvements

--- build.sh
+++ build.sh
@@ -1,154 +1,149 @@
 #!/bin/bash -e
 # shellcheck disable=SC2119
-run_sub_stage()
-{
-	log "Begin ${SUB_STAGE_DIR}"
-	pushd "${SUB_STAGE_DIR}" > /dev/null
-	for i in {00..99}; do
-		if [ -f "${i}-debconf" ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-debconf"
-			on_chroot << EOF
+run_sub_stage() {
+  log "Begin ${SUB_STAGE_DIR}"
+  pushd "${SUB_STAGE_DIR}" >/dev/null
+  for i in {00..99}; do
+    if [ -f "${i}-debconf" ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-debconf"
+      on_chroot <<EOF
 debconf-set-selections <<SELEOF
 $(cat "${i}-debconf")
 SELEOF
 EOF
 
-		log "End ${SUB_STAGE_DIR}/${i}-debconf"
-		fi
-		if [ -f "${i}-packages-nr" ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-packages-nr"
-			PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < "${i}-packages-nr")"
-			if [ -n "$PACKAGES" ]; then
-				on_chroot << EOF
+      log "End ${SUB_STAGE_DIR}/${i}-debconf"
+    fi
+    if [ -f "${i}-packages-nr" ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-packages-nr"
+      PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" <"${i}-packages-nr")"
+      if [ -n "$PACKAGES" ]; then
+        on_chroot <<EOF
 apt-get -o APT::Acquire::Retries=3 install --no-install-recommends -y $PACKAGES
 EOF
-			fi
-			log "End ${SUB_STAGE_DIR}/${i}-packages-nr"
-		fi
-		if [ -f "${i}-packages" ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-packages"
-			PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" < "${i}-packages")"
-			if [ -n "$PACKAGES" ]; then
-				on_chroot << EOF
+      fi
+      log "End ${SUB_STAGE_DIR}/${i}-packages-nr"
+    fi
+    if [ -f "${i}-packages" ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-packages"
+      PACKAGES="$(sed -f "${SCRIPT_DIR}/remove-comments.sed" <"${i}-packages")"
+      if [ -n "$PACKAGES" ]; then
+        on_chroot <<EOF
 apt-get -o APT::Acquire::Retries=3 install -y $PACKAGES
 EOF
-			fi
-			log "End ${SUB_STAGE_DIR}/${i}-packages"
-		fi
-		if [ -d "${i}-patches" ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-patches"
-			pushd "${STAGE_WORK_DIR}" > /dev/null
-			if [ "${CLEAN}" = "1" ]; then
-				rm -rf .pc
-				rm -rf ./*-pc
-			fi
-			QUILT_PATCHES="${SUB_STAGE_DIR}/${i}-patches"
-			SUB_STAGE_QUILT_PATCH_DIR="$(basename "$SUB_STAGE_DIR")-pc"
-			mkdir -p "$SUB_STAGE_QUILT_PATCH_DIR"
-			ln -snf "$SUB_STAGE_QUILT_PATCH_DIR" .pc
-			quilt upgrade
-			if [ -e "${SUB_STAGE_DIR}/${i}-patches/EDIT" ]; then
-				echo "Dropping into bash to edit patches..."
-				bash
-			fi
-			RC=0
-			quilt push -a || RC=$?
-			case "$RC" in
-				0|2)
-					;;
-				*)
-					false
-					;;
-			esac
-			popd > /dev/null
-			log "End ${SUB_STAGE_DIR}/${i}-patches"
-		fi
-		if [ -x ${i}-run.sh ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-run.sh"
-			./${i}-run.sh
-			log "End ${SUB_STAGE_DIR}/${i}-run.sh"
-		fi
-		if [ -f ${i}-run-chroot.sh ]; then
-			log "Begin ${SUB_STAGE_DIR}/${i}-run-chroot.sh"
-			on_chroot < ${i}-run-chroot.sh
-			log "End ${SUB_STAGE_DIR}/${i}-run-chroot.sh"
-		fi
-	done
-	popd > /dev/null
-	log "End ${SUB_STAGE_DIR}"
+      fi
+      log "End ${SUB_STAGE_DIR}/${i}-packages"
+    fi
+    if [ -d "${i}-patches" ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-patches"
+      pushd "${STAGE_WORK_DIR}" >/dev/null
+      if [ "${CLEAN}" = "1" ]; then
+        rm -rf .pc
+        rm -rf ./*-pc
+      fi
+      QUILT_PATCHES="${SUB_STAGE_DIR}/${i}-patches"
+      SUB_STAGE_QUILT_PATCH_DIR="$(basename "$SUB_STAGE_DIR")-pc"
+      mkdir -p "$SUB_STAGE_QUILT_PATCH_DIR"
+      ln -snf "$SUB_STAGE_QUILT_PATCH_DIR" .pc
+      quilt upgrade
+      if [ -e "${SUB_STAGE_DIR}/${i}-patches/EDIT" ]; then
+        echo "Dropping into bash to edit patches..."
+        bash
+      fi
+      RC=0
+      quilt push -a || RC=$?
+      case "$RC" in
+      0 | 2) ;;
+      *)
+        false
+        ;;
+      esac
+      popd >/dev/null
+      log "End ${SUB_STAGE_DIR}/${i}-patches"
+    fi
+    if [ -x "${i}"-run.sh ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-run.sh"
+      ./"${i}"-run.sh
+      log "End ${SUB_STAGE_DIR}/${i}-run.sh"
+    fi
+    if [ -f ${i}-run-chroot.sh ]; then
+      log "Begin ${SUB_STAGE_DIR}/${i}-run-chroot.sh"
+      on_chroot <"${i}"-run-chroot.sh
+      log "End ${SUB_STAGE_DIR}/${i}-run-chroot.sh"
+    fi
+  done
+  popd >/dev/null
+  log "End ${SUB_STAGE_DIR}"
 }
 
-
-run_stage(){
-	log "Begin ${STAGE_DIR}"
-	STAGE="$(basename "${STAGE_DIR}")"
-	pushd "${STAGE_DIR}" > /dev/null
-	unmount "${WORK_DIR}/${STAGE}"
-	STAGE_WORK_DIR="${WORK_DIR}/${STAGE}"
-	ROOTFS_DIR="${STAGE_WORK_DIR}"/rootfs
-	if [ ! -f SKIP_IMAGES ]; then
-		if [ -f "${STAGE_DIR}/EXPORT_IMAGE" ]; then
-			EXPORT_DIRS="${EXPORT_DIRS} ${STAGE_DIR}"
-		fi
-	fi
-	if [ ! -f SKIP ]; then
-		if [ "${CLEAN}" = "1" ]; then
-			if [ -d "${ROOTFS_DIR}" ]; then
-				rm -rf "${ROOTFS_DIR}"
-			fi
-		fi
-		if [ -x prerun.sh ]; then
-			log "Begin ${STAGE_DIR}/prerun.sh"
-			./prerun.sh
-			log "End ${STAGE_DIR}/prerun.sh"
-		fi
-		for SUB_STAGE_DIR in "${STAGE_DIR}"/*; do
-			if [ -d "${SUB_STAGE_DIR}" ] &&
-			   [ ! -f "${SUB_STAGE_DIR}/SKIP" ]; then
-				run_sub_stage
-			fi
-		done
-	fi
-	unmount "${WORK_DIR}/${STAGE}"
-	PREV_STAGE="${STAGE}"
-	PREV_STAGE_DIR="${STAGE_DIR}"
-	PREV_ROOTFS_DIR="${ROOTFS_DIR}"
-	popd > /dev/null
-	log "End ${STAGE_DIR}"
+run_stage() {
+  log "Begin ${STAGE_DIR}"
+  STAGE="$(basename "${STAGE_DIR}")"
+  pushd "${STAGE_DIR}" >/dev/null
+  unmount "${WORK_DIR}/${STAGE}"
+  STAGE_WORK_DIR="${WORK_DIR}/${STAGE}"
+  ROOTFS_DIR="${STAGE_WORK_DIR}"/rootfs
+  if [ ! -f SKIP_IMAGES ]; then
+    if [ -f "${STAGE_DIR}/EXPORT_IMAGE" ]; then
+      EXPORT_DIRS="${EXPORT_DIRS} ${STAGE_DIR}"
+    fi
+  fi
+  if [ ! -f SKIP ]; then
+    if [ "${CLEAN}" = "1" ]; then
+      if [ -d "${ROOTFS_DIR}" ]; then
+        rm -rf "${ROOTFS_DIR}"
+      fi
+    fi
+    if [ -x prerun.sh ]; then
+      log "Begin ${STAGE_DIR}/prerun.sh"
+      ./prerun.sh
+      log "End ${STAGE_DIR}/prerun.sh"
+    fi
+    for SUB_STAGE_DIR in "${STAGE_DIR}"/*; do
+      if [ -d "${SUB_STAGE_DIR}" ] &&
+        [ ! -f "${SUB_STAGE_DIR}/SKIP" ]; then
+        run_sub_stage
+      fi
+    done
+  fi
+  unmount "${WORK_DIR}/${STAGE}"
+  PREV_STAGE="${STAGE}"
+  PREV_STAGE_DIR="${STAGE_DIR}"
+  PREV_ROOTFS_DIR="${ROOTFS_DIR}"
+  popd >/dev/null
+  log "End ${STAGE_DIR}"
 }
 
 if [ "$(id -u)" != "0" ]; then
-	echo "Please run as root" 1>&2
-	exit 1
+  echo "Please run as root" 1>&2
+  exit 1
 fi
 
 BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export BASE_DIR
 
 if [ -f router-config ]; then
-	# shellcheck disable=SC1091
-	source router-config
+  # shellcheck disable=SC1091
+  source router-config
 fi
 
-while getopts "c:" flag
-do
-	case "$flag" in
-		c)
-			EXTRA_CONFIG="$OPTARG"
-			# shellcheck disable=SC1090
-			source "$EXTRA_CONFIG"
-			;;
-		*)
-			;;
-	esac
+while getopts "c:" flag; do
+  case "$flag" in
+  c)
+    EXTRA_CONFIG="$OPTARG"
+    # shellcheck disable=SC1090
+    source "$EXTRA_CONFIG"
+    ;;
+  *) ;;
+  esac
 done
 
 export PI_ROUTER=${PI_ROUTER:-pi-router}
 export PI_ROUTER_REPO=${PI_ROUTER_REPO:-https://github.com/mahsank/pi-router}
 
 if [ -z "${IMG_NAME}" ]; then
-	echo "IMG_NAME not set" 1>&2
-	exit 1
+  echo "IMG_NAME not set" 1>&2
+  exit 1
 fi
 
 export USE_QEMU="${USE_QEMU:-0}"
@@ -211,23 +206,23 @@ source "${SCRIPT_DIR}/common"
 
 #check username is valid
 if [[ ! "$FIRST_USER_NAME" =~ ^[a-z][-a-z0-9_]*$ ]]; then
-	echo "Invalid FIRST_USER_NAME: $FIRST_USER_NAME"
-	exit 1
+  echo "Invalid FIRST_USER_NAME: $FIRST_USER_NAME"
+  exit 1
 fi
 
-if [[ -n "${APT_PROXY}" ]] && ! curl --silent "${APT_PROXY}" >/dev/null ; then
-	echo "Could not reach APT_PROXY server: ${APT_PROXY}"
-	exit 1
+if [[ -n "${APT_PROXY}" ]] && ! curl --silent "${APT_PROXY}" >/dev/null; then
+  echo "Could not reach APT_PROXY server: ${APT_PROXY}"
+  exit 1
 fi
 
-if [[ -n "${WPA_PASSWORD}" && ${#WPA_PASSWORD} -lt 8 || ${#WPA_PASSWORD} -gt 63  ]] ; then
-	echo "WPA_PASSWORD" must be between 8 and 63 characters
-	exit 1
+if [[ -n "${WPA_PASSWORD}" && ${#WPA_PASSWORD} -lt 8 || ${#WPA_PASSWORD} -gt 63 ]]; then
+  echo "WPA_PASSWORD" must be between 8 and 63 characters
+  exit 1
 fi
 
 if [[ "${PUBKEY_ONLY_SSH}" = "1" && -z "${PUBKEY_SSH_FIRST_USER}" ]]; then
-	echo "Must set 'PUBKEY_SSH_FIRST_USER' to a valid SSH public key if using PUBKEY_ONLY_SSH"
-	exit 1
+  echo "Must set 'PUBKEY_SSH_FIRST_USER' to a valid SSH public key if using PUBKEY_ONLY_SSH"
+  exit 1
 fi
 
 mkdir -p "${WORK_DIR}"
@@ -236,32 +231,32 @@ log "Begin ${BASE_DIR}"
 STAGE_LIST=${STAGE_LIST:-${BASE_DIR}/stage*}
 
 for STAGE_DIR in $STAGE_LIST; do
-	STAGE_DIR=$(realpath "${STAGE_DIR}")
-	run_stage
+  STAGE_DIR=$(realpath "${STAGE_DIR}")
+  run_stage
 done
 
 CLEAN=1
 for EXPORT_DIR in ${EXPORT_DIRS}; do
-	STAGE_DIR=${BASE_DIR}/export-image
-	# shellcheck source=/dev/null
-	source "${EXPORT_DIR}/EXPORT_IMAGE"
-	EXPORT_ROOTFS_DIR=${WORK_DIR}/$(basename "${EXPORT_DIR}")/rootfs
-	run_stage
-	if [ "${USE_QEMU}" != "1" ]; then
-		if [ -e "${EXPORT_DIR}/EXPORT_NOOBS" ]; then
-			# shellcheck source=/dev/null
-			source "${EXPORT_DIR}/EXPORT_NOOBS"
-			STAGE_DIR="${BASE_DIR}/export-noobs"
-			run_stage
-		fi
-	fi
+  STAGE_DIR=${BASE_DIR}/export-image
+  # shellcheck source=/dev/null
+  source "${EXPORT_DIR}/EXPORT_IMAGE"
+  EXPORT_ROOTFS_DIR=${WORK_DIR}/$(basename "${EXPORT_DIR}")/rootfs
+  run_stage
+  if [ "${USE_QEMU}" != "1" ]; then
+    if [ -e "${EXPORT_DIR}/EXPORT_NOOBS" ]; then
+      # shellcheck source=/dev/null
+      source "${EXPORT_DIR}/EXPORT_NOOBS"
+      STAGE_DIR="${BASE_DIR}/export-noobs"
+      run_stage
+    fi
+  fi
 done
 
 if [ -x ${BASE_DIR}/postrun.sh ]; then
-	log "Begin postrun.sh"
-	cd "${BASE_DIR}"
-	./postrun.sh
-	log "End postrun.sh"
+  log "Begin postrun.sh"
+  cd "${BASE_DIR}"
+  ./postrun.sh
+  log "End postrun.sh"
 fi
 
 log "End ${BASE_DIR}"
--- stage2/05-enforce-pwd-change/04-run.sh
+++ stage2/05-enforce-pwd-change/04-run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-
+on_chroot <<EOF
 chage --lastday 1970-01-01 "${FIRST_USER_NAME}"
-
 EOF
